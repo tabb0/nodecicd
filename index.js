3fc4f375c936beb49d4ff38aaffe358f662d3e4e
Gustavo Rodriguez <gustavor@loymark.com>
2019-05-17 16:13:34 -0600

    asdfa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-    console.log("This is the debug test new with more details....")
+    console.log("This is the debug test new with more details....Preuba")
     res.send('hey world version 2');
 });
 //listen to port 3000 by default
